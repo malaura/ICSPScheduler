3e3b218ead104d29bd31827153d116f64511e384
Syd <slynch2@uoregon.edu>
2017-11-25 00:48:47 -0800

    add scrollbars, fix add/remove

--- code/gui/MainWindow.py
+++ code/gui/MainWindow.py
@@ -17,7 +17,7 @@ class MainWindow():
         '''
         self.root = root
         self.root.title("ICSPS Scheduler")
-        self.width = 800
+        self.width = 850
         self.height = 350
         self.root.minsize(height=self.height, width=self.width)
         self.root.maxsize(height=self.height, width=self.width)
@@ -67,13 +67,16 @@ class MainWindow():
 
         # Initalize left frame widgets
         requestLabel = Label(self.leftFrame, text="Request", background="gray90")
-        requestLabel.grid(column=0, row=0, columnspan=2)
-        requestView = Listbox(self.leftFrame, height=17, width=20)
-        requestView.grid(row=1, column=0, padx=5, columnspan=2)
+        requestLabel.grid(column=1, row=0, columnspan=2)
+        requestScroll = Scrollbar(self.leftFrame, orient=VERTICAL)
+        requestView = Listbox(self.leftFrame, height=17, width=20, selectmode=SINGLE, yscrollcommand=requestScroll.set)
+        requestScroll.config(command=requestView.yview)
+        requestScroll.grid(row=1, column=0, sticky="NS")
+        requestView.grid(row=1, column=1, padx=5, columnspan=2)
         editButton = ttk.Button(self.leftFrame, text="Edit", width=8)
-        editButton.grid(row=2, column=0, sticky=W, padx=(5,0))
+        editButton.grid(row=2, column=1, sticky=W, padx=(5,0))
         createButton = ttk.Button(self.leftFrame, text="Create", width=8, command=self.createNewPrompt)
-        createButton.grid(column=1, row=2)
+        createButton.grid(column=2, row=2)
 
         # Initalize central frame widgets
         appHighlightFont = font.Font(family='Helvetica', size=14, weight='bold')
@@ -122,18 +125,18 @@ class MainWindow():
         style = ttk.Style()
         style.configure("Blue.TButton", foreground="blue")
         for button in self.buttons:
+            button.configure(style="default.TButton")
             for request in MainCalendar.load_all_requests():
-                print(button['text'].strip()+"/"+str(self.currentYear))
-                print(request)
                 if button['text'].strip()+"/"+str(self.currentYear) == request:
                     button.configure(style="Blue.TButton")
-                else:
-                    button.configure(style="default.TButton")
-                    #pass
+
         # Initialize right frame widgets
         studentLabel = Label(self.rightFrame, text="Students", background="gray90")
         studentLabel.grid(row=0, column=0, columnspan=2)
-        studentView = Listbox(self.rightFrame, height=17, width=20)
+        studentScroll = Scrollbar(self.rightFrame, orient=VERTICAL)
+        studentView = Listbox(self.rightFrame, height=17, width=20, yscrollcommand=requestScroll.set, selectmode=SINGLE)
+        studentScroll.config(command=studentView.yview)
+        studentScroll.grid(row=1, column=2, sticky="NS")
         for student in self.students:
             studentView.insert(END, student)
 
@@ -151,6 +154,7 @@ class MainWindow():
 
         :return:
         '''
+        # Initialize labels
         self.prompt = Toplevel(self.root)
         self.prompt.title("Create New Request")
         self.prompt.configure(background = "gray90")
@@ -172,9 +176,10 @@ class MainWindow():
         bufferEnd = ttk.Label(self.prompt, text="Buffer Time After")
         bufferEnd.grid(row=7, column=0, sticky="e")
 
-        self.nameInput = ttk.Entry(self.prompt, width=20)
-        self.nameInput.grid(row=1, column=1, columnspan=2, padx=10)
-        self.monthInput = ttk.Combobox(self.prompt, width=17, state="readonly")
+        #Initalize inputs
+        self.nameInput = ttk.Entry(self.prompt, width=30)
+        self.nameInput.grid(row=1, column=1, columnspan=2)
+        self.monthInput = ttk.Combobox(self.prompt, width=27, state="readonly")
         months = []
         for i in range(1,13):
             if i < 10:
@@ -182,8 +187,8 @@ class MainWindow():
             else:
                 months.append(str(i))
         self.monthInput['values'] = months
-        self.monthInput.grid(row=2, column=1, columnspan=2, padx=10)
-        self.dayInput = ttk.Combobox(self.prompt, width=17, state="readonly")
+        self.monthInput.grid(row=2, column=1, columnspan=2)
+        self.dayInput = ttk.Combobox(self.prompt, width=27, state="readonly")
         days = []
         for i in range(1, 32):
             if i < 10:
@@ -191,9 +196,9 @@ class MainWindow():
             else:
                 days.append(str(i))
         self.dayInput['values'] = days
-        self.dayInput.grid(row=3, column=1, columnspan=2, padx=10)
+        self.dayInput.grid(row=3, column=1, columnspan=2)
         self.startHourInput = ttk.Combobox(self.prompt, width=8, state="readonly")
-        self.startHourInput.grid(row=4, column=1, padx=(75,0))
+        self.startHourInput.grid(row=4, column=1, sticky="E")
         hours = []
         for i in range(23):
             if i < 10:
@@ -216,8 +221,8 @@ class MainWindow():
         self.endMinuteInput = ttk.Combobox(self.prompt, width=8, state="readonly")
         self.endMinuteInput.grid(row=5, column=2, sticky="W")
         self.endMinuteInput['values'] = times
-        self.bufferStartInput = ttk.Combobox(self.prompt, width=17, state="readonly")
-        self.bufferStartInput.grid(row=6, column=1, columnspan=2, padx=10)
+        self.bufferStartInput = ttk.Combobox(self.prompt, width=27, state="readonly")
+        self.bufferStartInput.grid(row=6, column=1, columnspan=2)
         bufferList = []
         for i in range(0, 65, 5):
             if i < 10:
@@ -225,16 +230,20 @@ class MainWindow():
             else:
                 bufferList.append(str(i))
         self.bufferStartInput['values'] = bufferList
-        self.bufferEndInput = ttk.Combobox(self.prompt, width=17, state="readonly")
-        self.bufferEndInput.grid(row=7, column=1, columnspan=2, padx=10)
+        self.bufferEndInput = ttk.Combobox(self.prompt, width=27, state="readonly")
+        self.bufferEndInput.grid(row=7, column=1, columnspan=2)
         self.bufferEndInput['values'] = bufferList
+
+        #Initalize list views
         assignedLabel = ttk.Label(self.prompt, text="Assigned")
         assignedLabel.grid(row=8, column=0, pady=5)
-        self.assignedView = Listbox(self.prompt, width=20, height=10, selectmode=SINGLE)
+        assignedScroll = Scrollbar(self.prompt, orient=VERTICAL)
+        availableScroll = Scrollbar(self.prompt, orient=VERTICAL)
+        self.assignedView = Listbox(self.prompt, width=15, height=10, selectmode=SINGLE, yscrollcommand=assignedScroll.set)
         self.assignedView.grid(row=9, column=0, padx=(5,0), pady=5, sticky="W", rowspan=2)
-        availableLabel = ttk.Label(self.prompt, text="Available Students")
+        availableLabel = ttk.Label(self.prompt, text="Available")
         availableLabel.grid(row=8, column=2, pady=5)
-        self.availableView = Listbox(self.prompt, width=20, height=10, selectmode=SINGLE)
+        self.availableView = Listbox(self.prompt, width=15, height=10, yscrollcommand=availableScroll.set, selectmode=SINGLE)
         self.availableView.grid(row=9, column=2, rowspan=2, padx=(0,5), pady=5, sticky="E")
 
         leftButton = ttk.Button(self.prompt, text="<", command=self.assignStudent)
@@ -262,15 +271,13 @@ class MainWindow():
                   str(self.endHourInput.get())+":"+str(self.endMinuteInput.get()),
                   "0:"+str(self.bufferStartInput.get()),
                   "0:"+str(self.bufferEndInput.get()))
-        #self.requests.add_request(request)
+
         self.availableStudents = MainCalendar.find_available_students(self.students, self.request)
         print(self.availableStudents)
         self.availableView.delete(0, END)
         for student in self.availableStudents:
-            print(type(student))
             print(student)
             self.availableView.insert(END, student)
-        #print(load_all_requests())
 
 
     def assignStudent(self):
@@ -278,6 +285,7 @@ class MainWindow():
         self.availableView.delete(self.availableView.curselection()[0])
 
     def removeStudent(self):
+        self.availableView.insert(END, self.assignedView[self.assignedView.curselection()[0]])
         self.assignedView.delete(self.assignedView.curselection()[0])
 
     def confirmRequest(self):
@@ -287,7 +295,7 @@ class MainWindow():
         print(type(self.request))
         self.requests.add_request(self.request)
         for student in self.assignedView.get(0, END):
-            set_student_to_request(studentSchedules[student], self.request)
+            MainCalendar.set_student_to_request(studentSchedules[student], self.request)
         self.prompt.destroy()
         self.updateCalendar()
 
@@ -330,12 +338,10 @@ class MainWindow():
         style = ttk.Style()
         style.configure("Blue.TButton", foreground="blue")
         for button in self.buttons:
+            button.configure(style="default.TButton")
             for request in MainCalendar.load_all_requests():
-                print(button['text'].strip()+"/"+str(self.currentYear))
-                print(request)
                 if button['text'].strip()+"/"+str(self.currentYear) == request:
                     button.configure(style="Blue.TButton")
-                else:
-                    button.configure(style="default.TButton")
-                    #pass
+
+
         self.monthLabel.configure(text=self.months[self.currentMonth-1]+" "+str(self.currentYear))
