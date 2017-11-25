a33c0c362ec2979eb83c6b6df7a27376be76d604
Jim Li <jinjiel1994@gmail.com>
2017-11-25 15:17:35 -0800

    coverage

---
+++ tests/testRequests.py
@@ -0,0 +1,46 @@
+from code import Requests
+import unittest
+
+
+class TestRequests(unittest.TestCase):
+
+    def test_request_get_name(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_name = request.get_name()
+        self.assertEqual(get_name, 'test')
+
+    def test_request_get_date(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_date = request.get_date()
+        self.assertEqual(get_date, '11/25/2017')
+
+    def test_request_get_start_time(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_start_time = request.get_start_time()
+        self.assertEqual(get_start_time, '00:00')
+
+    def test_request_get_end_time(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_end_time = request.get_end_time()
+        self.assertEqual(get_end_time, '02:00')
+
+    def test_request_get_buffer_start(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_buffer_start = request.get_buffer_start()
+        self.assertEqual(get_buffer_start, '00:00')
+
+    def test_request_get_buffer_end(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_buffer_end = request.get_buffer_end()
+        self.assertEqual(get_buffer_end, '00:00')
+
+    def test_request_get_actual_start_time(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_actual_start_time = request.get_actual_start_time()
+        self.assertEqual(get_actual_start_time, '00:00')
+
+    def test_request_get_actual_end_time(self):
+        request = Requests.Request('test', '11/25/2017', '00:00', '02:00', '00:00', '00:00')
+        get_actual_end_time = request.get_actual_end_time()
+        self.assertEqual(get_actual_end_time, '02:00')
+
--- tests/testStudent.py
+++ tests/testStudent.py
@@ -4,13 +4,38 @@ import unittest
 
 class TestStudent(unittest.TestCase):
     def test_get_directory(self):
-        mockDirectory = 'Students/Jim.csv'
+        mockDirectory = 'test.csv'
         student = Student(mockDirectory)
         directory = student.get_directory()
         self.assertEqual(directory, mockDirectory)
 
     def test_get_student_name(self):
-        mockDirectory = 'Students/Jim.csv'
+        mockDirectory = 'test.csv'
         student = Student(mockDirectory)
         name = student.get_student_name()
-        self.assertEqual(name, 'Jim')
+        self.assertEqual(name, 'test')
+
+    def test_get_validation_info(self):
+        mockDirectory = 'test.csv'
+        student = Student(mockDirectory)
+        validation_info = student.get_validation_info()
+        self.assertEqual(validation_info, "Format is correct")
+
+    def test_get_validation(self):
+        mockDirectory = 'test.csv'
+        student = Student(mockDirectory)
+        get_validation = student.get_validation()
+        self.assertEqual(get_validation, True)
+
+    def test_get_dictionary_of_schedule(self):
+        mockDirectory = 'test.csv'
+        student = Student(mockDirectory)
+        get_dictionary_of_schedule = student.get_dictionary_of_schedule()
+        self.assertEqual(get_dictionary_of_schedule, {})
+
+    def test_get_dictionary_of_time_interval(self):
+        mockDirectory = 'test.csv'
+        student = Student(mockDirectory)
+        get_dictionary_of_time_interval = student.get_dictionary_of_time_interval()
+        self.assertEqual(get_dictionary_of_time_interval, {})
+
